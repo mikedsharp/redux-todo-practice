9045538f1c71a2452963573722fabad155338546
Mike Sharp <msharp23@hotmail.co.uk>
2018-01-07 14:34:00 +0000

    added todo reducers and actions, and some tests

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,44 @@
+export var searchTextReducer = (state = '', action) => {
+    switch(action.type) {
+        case 'SET_SEARCH_TEXT': {
+            return action.searchText;
+        }
+        default: {
+            return state;    
+        }
+    }
+};
+
+export var showCompletedReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'TOGGLE_SHOW_COMPLETED': {
+            return !state;
+        }
+        default: {
+            return state;    
+        }
+    }
+};
+/*
+export var addTodoReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'TOGGLE_SHOW_COMPLETED': {
+            return action.searchText;
+        }
+        default: {
+            return state;    
+        }
+    }
+}
+
+export var toggleTodoReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'TOGGLE_SHOW_COMPLETED': {
+            return action.searchText;
+        }
+        default: {
+            return state;    
+        }
+    }
+}
+*/
